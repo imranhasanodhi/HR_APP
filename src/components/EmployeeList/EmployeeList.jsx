f026313f125b4660b23ccef8646f660437f836fc
Imran Hasan <imranhasanodhi1@gmail.com>
2024-11-06 12:24:20 +0200

    add 2nd week 1st step

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,14 +1,30 @@
 import EmployeeCard from "../EmployeeCard/EmployeeCard.jsx";
 import "./EmployeeList.css";
 
+const employees = [
+  { id: 1, name: "Margit", initRole: "teacher", department: "ICT" },
+  { id: 2, name: "Vera", initRole: "teacher", department: "ICT" },
+  { id: 3, name: "Ilkka", initRole: "teacher", department: "ICT" },
+  { id: 4, name: "Joonas", initRole: "teacher", department: "ICT" },
+  { id: 5, name: "Santosh", initRole: "teacher", department: "ICT" },
+  { id: 6, name: "Alice", initRole: "developer", department: "IT" },
+  { id: 7, name: "Bob", initRole: "designer", department: "Design" },
+  { id: 8, name: "Claire", initRole: "manager", department: "HR" },
+  { id: 9, name: "Diana", initRole: "developer", department: "IT" },
+  { id: 10, name: "Eva", initRole: "HR specialist", department: "HR" },
+];
+
 const EmployeeList = () => {
   return (
     <div className="list">
-      <EmployeeCard name="Margit" initRole="teacher" department="ICT" />
-      <EmployeeCard name="Vera" initRole="teacher" department="ICT" />
-      <EmployeeCard name="Ilkka" initRole="teacher" department="ICT" />
-      <EmployeeCard name="Joonas" initRole="teacher" department="ICT" />
-      <EmployeeCard name="Santosh" initRole="teacher" department="ICT" />
+      {employees.map((employee) => (
+        <EmployeeCard
+          key={employee.id}
+          name={employee.name}
+          initRole={employee.initRole}
+          department={employee.department}
+        />
+      ))}
     </div>
   );
 };
